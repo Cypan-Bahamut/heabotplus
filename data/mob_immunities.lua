7135ceeb4f752dde9d6c2edd78cf6b7f3d45976c
Cypan-Bahamut <88352184+Cypan-Bahamut@users.noreply.github.com>
2022-08-22 08:14:44 +0900

    Add files via upload

---
+++ data/mob_immunities.lua
@@ -0,0 +1,345 @@
+return {
+    ["Aegis"] = S{
+        28
+    },
+    ["Agon Halo"] = S{
+        4
+    },
+    ["Agon Shieldsaint"] = S{
+        7
+    },
+    ["Air Circle"] = S{
+        10
+    },
+    ["Amanomurakumo"] = S{
+        28
+    },
+    ["Amaranth Barrier"] = S{
+        194
+    },
+    ["Animated Longbow"] = S{
+        10
+    },
+    ["Annihilative Adenium"] = S{
+        2
+    },
+    ["Arch Dynamis Lord"] = S{
+        2
+    },
+    ["Archaic Gear"] = S{
+        2
+    },
+    ["Arebati"] = S{
+        28
+    },
+    ["Assailer Chariot"] = S{
+        6
+    },
+    ["Bedrock Crag"] = S{
+        4,
+        194
+    },
+    ["Blazenought"] = S{
+        10
+    },
+    ["Bozzetto Berserker"] = S{
+        28
+    },
+    ["Bozzetto Consul"] = S{
+        28
+    },
+    ["Bozzetto Deathsinger"] = S{
+        2,
+        28
+    },
+    ["Bozzetto Elemental"] = S{
+        2
+    },
+    ["Bozzetto Fenrir"] = S{
+        2,
+        5,
+        7,
+        28
+    },
+    ["Bozzetto Garuda"] = S{
+        2,
+        13,
+        194
+    },
+    ["Bozzetto High Vicar"] = S{
+        28
+    },
+    ["Bozzetto Hunter"] = S{
+        2
+    },
+    ["Bozzetto Ifrit"] = S{
+        4,
+        10,
+        11,
+        194
+    },
+    ["Bozzetto Leviathan"] = S{
+        2,
+        4,
+        6,
+        7,
+        11,
+        13,
+        194
+    },
+    ["Bozzetto Necronura"] = S{
+        7
+    },
+    ["Bozzetto Ramuh"] = S{
+        3,
+        4,
+        5,
+        10
+    },
+    ["Bozzetto Shiva"] = S{
+        2,
+        4,
+        7,
+        10,
+        194
+    },
+    ["Bozzetto Songstress"] = S{
+        2
+    },
+    ["Bozzetto Stalwart"] = S{
+        2
+    },
+    ["Bozzetto Titan"] = S{
+        2,
+        4,
+        194
+    },
+    ["Bravura"] = S{
+        28
+    },
+    ["Broadleaf Palm"] = S{
+        2,
+        194
+    },
+    ["Brulo"] = S{
+        2
+    },
+    ["Chest O'Plenty"] = S{
+        6
+    },
+    ["Chigre"] = S{
+        2,
+        7
+    },
+    ["Cloud of Darkness"] = S{
+        7
+    },
+    ["Corporal Tombstone"] = S{
+        2
+    },
+    ["Cuijatender"] = S{
+        2,
+        6
+    },
+    ["Dark Circle"] = S{
+        4,
+        10
+    },
+    ["Dark Gyves"] = S{
+        4,
+        194
+    },
+    ["Despot"] = S{
+        194
+    },
+    ["Devilet"] = S{
+        2
+    },
+    ["Dhorme Khimaira"] = S{
+        13
+    },
+    ["Dimensional Tether"] = S{
+        194
+    },
+    ["Earth Circle"] = S{
+        4,
+        10
+    },
+    ["Earth Elemental"] = S{
+        2
+    },
+    ["Evincing Idol"] = S{
+        6,
+        11,
+        28
+    },
+    ["Excalibur"] = S{
+        28
+    },
+    ["Fe'e"] = S{
+        13,
+        194
+    },
+    ["Fire Circle"] = S{
+        10
+    },
+    ["Flame Skimmer"] = S{
+        2
+    },
+    ["Frostnought"] = S{
+        10
+    },
+    ["Fu"] = S{
+        10,
+        11,
+        12
+    },
+    ["Gin"] = S{
+        6,
+        10
+    },
+    ["Glassy Craver"] = S{
+        2,
+        6,
+        10
+    },
+    ["Glassy Gorger"] = S{
+        7,
+        10
+    },
+    ["Glassy Thinker"] = S{
+        6,
+        10
+    },
+    ["Gnarled Rampart"] = S{
+        194
+    },
+    ["Guttler"] = S{
+        28
+    },
+    ["Harpimaira"] = S{
+        11,
+        12
+    },
+    ["Ice Circle"] = S{
+        10,
+        194
+    },
+    ["Ignamoth"] = S{
+        4
+    },
+    ["Iku-Turso"] = S{
+        194
+    },
+    ["Impish Statue"] = S{
+        2,
+        6
+    },
+    ["Kei"] = S{
+        6,
+        10,
+        11
+    },
+    ["Kikoku"] = S{
+        28
+    },
+    ["Kin"] = S{
+        6,
+        10,
+        12,
+        31
+    },
+    ["Knotted Root"] = S{
+        194
+    },
+    ["Kyou"] = S{
+        10,
+        12
+    },
+    ["Light Circle"] = S{
+        10
+    },
+    ["Lithicthrower Image"] = S{
+        2
+    },
+    ["Maere"] = S{
+        2
+    },
+    ["Mandau"] = S{
+        28
+    },
+    ["Marquis Caim"] = S{
+        2
+    },
+    ["Melo Melo"] = S{
+        2,
+        7,
+        11
+    },
+    ["Mjollnir"] = S{
+        28
+    },
+    ["Ogopogo"] = S{
+        2
+    },
+    ["Ou"] = S{
+        6,
+        10,
+        11,
+        12
+    },
+    ["Overseer's Tombstone"] = S{
+        6
+    },
+    ["Ragnarok"] = S{
+        28
+    },
+    ["Refitted Chariot"] = S{
+        2
+    },
+    ["Rencounter Chariot"] = S{
+        2
+    },
+    ["Sere Stump"] = S{
+        194
+    },
+    ["Sextuple Razool Ja"] = S{
+        10,
+        28
+    },
+    ["Shinryu"] = S{
+        28
+    },
+    ["Sovereign Behemoth"] = S{
+        6
+    },
+    ["Spharai"] = S{
+        28
+    },
+    ["Tapana"] = S{
+        2
+    },
+    ["Thunder Circle"] = S{
+        4,
+        10
+    },
+    ["Tinnin"] = S{
+        10
+    },
+    ["Tolba"] = S{
+        10,
+        194
+    },
+    ["Vidmapire"] = S{
+        11
+    },
+    ["Water Circle"] = S{
+        4,
+        10
+    },
+    ["Wintry Cave"] = S{
+        5,
+        13
+    },
+    ["_comment"] = "This file is automatically updated with buff IDs as immunities are discovered"
+}
